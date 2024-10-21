26a363e28dfee867dc47833dd1582c64e1e4653a
Tomas-2111 <tojoalgu@gmail.com>
2024-10-20 20:39:12 -0600

    Adición proyecto y readme.txt

---
+++ Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/Banco_Grupo5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.banco_grupo5;
+
+/**
+ *
+ * @author Tomás Alfaro
+ */
+public class Banco_Grupo5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
