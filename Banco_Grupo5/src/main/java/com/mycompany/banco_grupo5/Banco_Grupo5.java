bb57e10a1628896830d832b2d00257c86c8d9ed2
Tomas-2111 <tojoalgu@gmail.com>
2024-10-26 12:24:49 -0600

    Metodo validar usuario y bitacora

--- Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/Banco_Grupo5.java
+++ Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/Banco_Grupo5.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.banco_grupo5;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Tomás Alfaro
@@ -11,6 +13,22 @@ package com.mycompany.banco_grupo5;
 public class Banco_Grupo5 {
 
     public static void main(String[] args) {
+        Login login=new Login();
+        String user="";
+        String pass="";
         
+        int tipo=0;
+        while(tipo==0){
+            JOptionPane.showMessageDialog(null, "Bienvenido\nFavor ingrese el usuario y contraseña");
+            user=JOptionPane.showInputDialog("Ingrese el usuario");
+            pass=JOptionPane.showInputDialog("Ingrese la contraseña");
+            tipo=login.revisarLogin(user, pass);
+            if(tipo==0){
+                JOptionPane.showMessageDialog(null, "Usuario y/o contraseña incorrectos favor intente de nuevo");
+            }
+        }
+        
+        
+        JOptionPane.showMessageDialog(null, "Bienvenido\nFavor elija una opcion\n1. Ingresar Ticket\n2. Atender Tickets");
     }
 }
---
+++ Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/Login.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banco_grupo5;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.time.LocalDateTime;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Tomás Alfaro
+ */
+public class Login {
+    
+    private String user1="admin";
+    private String user2="user";
+    private String pass1="admin123";
+    private String pass2="user123";
+    
+    
+    public int revisarLogin(String user, String pass){//Revisa el login y retorna el tipo de usuario o 0 si es incorrecto
+        int tipo=0;
+        if(user.equals(user1) && pass.equals(pass1))
+        {
+            tipo=1;
+            
+        }else if(user.equals(user2) && pass.equals(pass2))
+        {
+            tipo=2;
+        }
+        ingresarBitacora(tipo,user);
+        return tipo;
+    }
+    
+    
+    private void ingresarBitacora(int tipo, String user)//Si el ingreso fue correcto lo guarda en bitacora
+    {
+        LocalDateTime locaDate = LocalDateTime.now();//Obtiene hora actual
+        int hours  = locaDate.getHour();
+        int minutes = locaDate.getMinute();
+        try{
+        //aquí creamos el archivo y la entrada de datos
+            DataOutputStream archivo= new DataOutputStream(new FileOutputStream("bitacora.txt", true));//El nombre del archivo que se va a crear cada que se agrega; El true concatena los datos           
+            if(tipo!=0)
+            {
+                //Si los datos están correctos los escribimos en el archivo
+                archivo.writeUTF("Inicio de sesion exitoso");//UTF es formato de texto enriquecido tipo Word
+                archivo.writeUTF("Hora: "+hours+":"+minutes);
+                archivo.writeUTF("Usuario: "+user);
+                //Debe cerrar ela rchivo una vez se deje utilizar
+                archivo.close();   
+            }
+            
+
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Error al agregar datos: "+e.getMessage(), "Error al agregar",JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    
+    
+}
