9cd4232a41c1277161366d448d22ffe5cf2a516e
Tomas-2111 <tojoalgu@gmail.com>
2024-11-28 21:50:26 -0600

    Serialización pt2

--- Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/GestionColas.java
+++ Banco_Grupo5/src/main/java/com/mycompany/banco_grupo5/GestionColas.java
@@ -14,11 +14,11 @@ import javax.swing.JOptionPane;
 public class GestionColas {
     Cola cola = new Cola();
     Serializacion serializacion= new Serializacion();
-    Cola colaNormall = serializacion.cargarCola("listaCola1.json")==null?new Cola():serializacion.cargarCola("listaCola1");
-    Cola colaNormal2 =serializacion.cargarCola("listaCola2.json")==null?new Cola():serializacion.cargarCola("listaCola2");
-    Cola colaNormal3 = new Cola();
+    Cola colaNormall = serializacion.cargarCola("listaCola1.json")==null?new Cola():serializacion.cargarCola("listaCola1.json");
+    Cola colaNormal2 =serializacion.cargarCola("listaCola2.json")==null?new Cola():serializacion.cargarCola("listaCola2.json");
+    Cola colaNormal3 = serializacion.cargarCola("listaCola3.json")==null?new Cola():serializacion.cargarCola("listaCola3.json");
     Cola colaPreferencial = serializacion.cargarCola("listaPreferencial.json")==null?new Cola():serializacion.cargarCola("listaPreferencial.json");
-    Cola colaTramiteRapido = new Cola();
+    Cola colaTramiteRapido = serializacion.cargarCola("listaRapida.json")==null?new Cola():serializacion.cargarCola("listaRapida.json");
     
     
     public void ingresarTicket(){
@@ -52,6 +52,7 @@ public class GestionColas {
                 break;
             case "a":
                 colaTramiteRapido.agregarTicket(nuevoTicket);
+                serializacion.guardarCola(colaTramiteRapido, "listaRapida.json");
                 numeroFila=cantidadRapida+1;
                 caja="Caja Rápida";
                 break;
@@ -59,17 +60,20 @@ public class GestionColas {
             default:
                 if(cantidadCola1<=cantidadCola2 && cantidadCola1<=cantidadCola3){
                     colaNormall.agregarTicket(nuevoTicket);
+                    serializacion.guardarCola(colaNormall, "listaCola1.json");
                     numeroFila=cantidadCola1+1;
                     caja="Caja 1";
                     break;
                     
                 }else  if(cantidadCola2<=cantidadCola1 && cantidadCola2<=cantidadCola3){
                     colaNormal2.agregarTicket(nuevoTicket);
+                    serializacion.guardarCola(colaNormal2, "listaCola2.json");
                     numeroFila=cantidadCola2+1;
                     caja="Caja 2";
                     break;
                 }else if(cantidadCola3<=cantidadCola1 && cantidadCola3<=cantidadCola2){
                     colaNormal3.agregarTicket(nuevoTicket);
+                    serializacion.guardarCola(colaNormal3, "listaCola3.json");
                     numeroFila=cantidadCola3+1;
                     caja="Caja 3";
                     break;
